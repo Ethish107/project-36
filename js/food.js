0542b7bb779848558f563e33617a33ae658b3902
Ethish107 <73375266+Ethish107@users.noreply.github.com>
2021-01-25 05:41:45 -0500

    Add files via upload

---
+++ js/food.js
@@ -0,0 +1,44 @@
+class Food
+{
+  constructor(){
+    this.foodS = database.ref('Food');
+    this.image = loadImage("Images/Milk.png");
+  }
+  getFoodStock(){
+    this.foodS.on("value",(data) => {
+      foodStock = data.val();
+      push()
+      fill(255);
+      textSize(20);
+      text("Food left : " + foodStock + " Milk Bottles",300,85);
+    });
+    pop()
+  }
+
+  updateFoodStock(changeVal){
+    database.ref().update({Food:foodStock - changeVal});
+  }
+
+  detectFood(){
+   if(foodStock!= 0){
+    foodStock--;
+   }
+  }
+  
+  display(){
+    var x = 40,y = 120;
+    if(foodStock!= 0){
+      for(var i = 0;i < foodStock; i++){
+
+        if(i%15 === 0){
+          y+= 50;
+          x = 40;
+        }
+
+        image(this.image,x,y,50,50);
+        x+=30;
+      }
+    }
+  }
+
+}
